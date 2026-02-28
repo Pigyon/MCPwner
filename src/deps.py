1256666d614b5870251b36d0d53f5031d6ca65b2
nedlir <neliran2@telem.openu.ac.il>
2026-02-28 20:31:50 +0200

    Refactor_DI_CodeQLService

--- src/deps.py
+++ src/deps.py
@@ -3,226 +3,172 @@
 Simple factory functions for service instantiation following MCP server conventions.
 """
 
+from functools import lru_cache
+
 from clients.codeql import CodeQLClient
 from clients.linguist import LinguistClient
 from clients.semgrep import SemgrepClient
+from clients.bandit import BanditClient
+from clients.gosec import GosecClient
+from clients.brakeman import BrakemanClient
+from clients.pmd import PMDClient
+from clients.psalm import PsalmClient
+
 from config.config import load_config
 from repositories.workspace import WorkspaceRepository
+
 from services.codeql import CodeQLService
 from services.linguist import LinguistService
 from services.semgrep import SemgrepService
+from services.bandit import BanditService
+from services.gosec import GosecService
+from services.brakeman import BrakemanService
+from services.pmd import PMDService
+from services.psalm import PsalmService
 from services.workspace import WorkspaceService
 
-# Global instances
-_config = None
-_workspace_repository = None
-_codeql_client = None
-_linguist_client = None
-_workspace_service = None
-_codeql_service = None
-_linguist_service = None
 
-# SAST tool instances
-_semgrep_client = None
-_semgrep_service = None
-_bandit_client = None
-_bandit_service = None
-_gosec_client = None
-_gosec_service = None
-_brakeman_client = None
-_brakeman_service = None
-_pmd_client = None
-_pmd_service = None
-_psalm_client = None
-_psalm_service = None
-
-
+@lru_cache(maxsize=None)
 def get_config():
     """Get configuration singleton."""
-    global _config
-    if _config is None:
-        _config = load_config("config/config.yaml")
-    return _config
+    return load_config("config/config.yaml")
 
 
+@lru_cache(maxsize=None)
 def get_workspace_repository():
     """Get workspace repository singleton."""
-    global _workspace_repository
-    if _workspace_repository is None:
-        _workspace_repository = WorkspaceRepository()
-    return _workspace_repository
+    return WorkspaceRepository()
 
 
+@lru_cache(maxsize=None)
 def get_codeql_client():
     """Get CodeQL client singleton."""
-    global _codeql_client
-    if _codeql_client is None:
-        config = get_config()
-        _codeql_client = CodeQLClient(config["codeql"]["service_url"])
-    return _codeql_client
+    config = get_config()
+    return CodeQLClient(config["codeql"]["service_url"])
 
 
+@lru_cache(maxsize=None)
 def get_linguist_client():
     """Get Linguist client singleton."""
-    global _linguist_client
-    if _linguist_client is None:
-        config = get_config()
-        _linguist_client = LinguistClient(config["linguist"]["service_url"])
-    return _linguist_client
+    config = get_config()
+    return LinguistClient(config["linguist"]["service_url"])
 
 
+@lru_cache(maxsize=None)
 def get_workspace_service():
     """Get workspace service singleton."""
-    global _workspace_service
-    if _workspace_service is None:
-        _workspace_service = WorkspaceService(get_workspace_repository())
-    return _workspace_service
+    return WorkspaceService(get_workspace_repository())
 
 
-def get_codeql_service():
-    """Get CodeQL service singleton."""
-    global _codeql_service
-    if _codeql_service is None:
-        _codeql_service = CodeQLService(get_workspace_repository(), get_codeql_client())
-    return _codeql_service
-
-
+@lru_cache(maxsize=None)
 def get_linguist_service():
     """Get Linguist service singleton."""
-    global _linguist_service
-    if _linguist_service is None:
-        _linguist_service = LinguistService(get_workspace_repository(), get_linguist_client())
-    return _linguist_service
+    return LinguistService(get_workspace_repository(), get_linguist_client())
 
 
-# SAST Service Getters
+@lru_cache(maxsize=None)
+def get_codeql_service():
+    """Get CodeQL service singleton."""
+    return CodeQLService(
+        get_workspace_repository(),
+        get_codeql_client(),
+        get_linguist_service()
+    )
 
 
+# SAST Clients and Services
+
+@lru_cache(maxsize=None)
+def get_semgrep_client():
+    config = get_config()
+    semgrep_url = config.get("semgrep", {}).get("service_url", "http://semgrep:8082")
+    return SemgrepClient(semgrep_url)
+
+
+@lru_cache(maxsize=None)
 def get_semgrep_service():
-    """Get Semgrep service singleton."""
-    global _semgrep_service, _semgrep_client
-    if _semgrep_service is None:
-        if _semgrep_client is None:
-            config = get_config()
-            semgrep_url = config.get("semgrep", {}).get("service_url", "http://semgrep:8082")
-            _semgrep_client = SemgrepClient(semgrep_url)
-
-        _semgrep_service = SemgrepService(get_workspace_repository(), _semgrep_client)
-    return _semgrep_service
+    return SemgrepService(get_workspace_repository(), get_semgrep_client())
 
 
+@lru_cache(maxsize=None)
+def get_bandit_client():
+    config = get_config()
+    bandit_url = config.get("bandit", {}).get("service_url", "http://bandit:8083")
+    return BanditClient(bandit_url)
+
+
+@lru_cache(maxsize=None)
 def get_bandit_service():
-    """Get Bandit service singleton."""
-    global _bandit_service, _bandit_client
-    if _bandit_service is None:
-        if _bandit_client is None:
-            config = get_config()
-            bandit_url = config.get("bandit", {}).get("service_url", "http://bandit:8083")
-            from clients.bandit import BanditClient
-
-            _bandit_client = BanditClient(bandit_url)
-
-        from services.bandit import BanditService
-
-        _bandit_service = BanditService(get_workspace_repository(), _bandit_client)
-    return _bandit_service
+    return BanditService(get_workspace_repository(), get_bandit_client())
 
 
+@lru_cache(maxsize=None)
+def get_gosec_client():
+    config = get_config()
+    gosec_url = config.get("gosec", {}).get("service_url", "http://gosec:8084")
+    return GosecClient(gosec_url)
+
+
+@lru_cache(maxsize=None)
 def get_gosec_service():
-    """Get Gosec service singleton."""
-    global _gosec_service, _gosec_client
-    if _gosec_service is None:
-        if _gosec_client is None:
-            config = get_config()
-            gosec_url = config.get("gosec", {}).get("service_url", "http://gosec:8084")
-            from clients.gosec import GosecClient
-
-            _gosec_client = GosecClient(gosec_url)
-
-        from services.gosec import GosecService
-
-        _gosec_service = GosecService(get_workspace_repository(), _gosec_client)
-    return _gosec_service
+    return GosecService(get_workspace_repository(), get_gosec_client())
 
 
+@lru_cache(maxsize=None)
+def get_brakeman_client():
+    config = get_config()
+    brakeman_url = config.get("brakeman", {}).get("service_url", "http://brakeman:8085")
+    return BrakemanClient(brakeman_url)
+
+
+@lru_cache(maxsize=None)
 def get_brakeman_service():
-    """Get Brakeman service singleton."""
-    global _brakeman_service, _brakeman_client
-    if _brakeman_service is None:
-        if _brakeman_client is None:
-            config = get_config()
-            brakeman_url = config.get("brakeman", {}).get("service_url", "http://brakeman:8085")
-            from clients.brakeman import BrakemanClient
-
-            _brakeman_client = BrakemanClient(brakeman_url)
-
-        from services.brakeman import BrakemanService
-
-        _brakeman_service = BrakemanService(get_workspace_repository(), _brakeman_client)
-    return _brakeman_service
+    return BrakemanService(get_workspace_repository(), get_brakeman_client())
 
 
+@lru_cache(maxsize=None)
+def get_pmd_client():
+    config = get_config()
+    pmd_url = config.get("pmd", {}).get("service_url", "http://pmd:8086")
+    return PMDClient(pmd_url)
+
+
+@lru_cache(maxsize=None)
 def get_pmd_service():
-    """Get PMD service singleton."""
-    global _pmd_service, _pmd_client
-    if _pmd_service is None:
-        if _pmd_client is None:
-            config = get_config()
-            pmd_url = config.get("pmd", {}).get("service_url", "http://pmd:8086")
-            from clients.pmd import PMDClient
-
-            _pmd_client = PMDClient(pmd_url)
-
-        from services.pmd import PMDService
-
-        _pmd_service = PMDService(get_workspace_repository(), _pmd_client)
-    return _pmd_service
+    return PMDService(get_workspace_repository(), get_pmd_client())
 
 
+@lru_cache(maxsize=None)
+def get_psalm_client():
+    config = get_config()
+    psalm_url = config.get("psalm", {}).get("service_url", "http://psalm:8087")
+    return PsalmClient(psalm_url)
+
+
+@lru_cache(maxsize=None)
 def get_psalm_service():
-    """Get Psalm service singleton."""
-    global _psalm_service, _psalm_client
-    if _psalm_service is None:
-        if _psalm_client is None:
-            config = get_config()
-            psalm_url = config.get("psalm", {}).get("service_url", "http://psalm:8087")
-            from clients.psalm import PsalmClient
-
-            _psalm_client = PsalmClient(psalm_url)
-
-        from services.psalm import PsalmService
-
-        _psalm_service = PsalmService(get_workspace_repository(), _psalm_client)
-    return _psalm_service
+    return PsalmService(get_workspace_repository(), get_psalm_client())
 
 
 def reset_dependencies():
     """Reset all dependencies (useful for testing)."""
-    global _config, _workspace_repository, _codeql_client, _linguist_client
-    global _workspace_service, _codeql_service, _linguist_service, _context_service
-    global _semgrep_client, _semgrep_service, _bandit_client, _bandit_service
-    global _gosec_client, _gosec_service, _brakeman_client, _brakeman_service
-    global _pmd_client, _pmd_service, _psalm_client, _psalm_service
-
-    _config = None
-    _workspace_repository = None
-    _codeql_client = None
-    _linguist_client = None
-    _workspace_service = None
-    _codeql_service = None
-    _linguist_service = None
-    _context_service = None
-
-    # Reset SAST tool instances
-    _semgrep_client = None
-    _semgrep_service = None
-    _bandit_client = None
-    _bandit_service = None
-    _gosec_client = None
-    _gosec_service = None
-    _brakeman_client = None
-    _brakeman_service = None
-    _pmd_client = None
-    _pmd_service = None
-    _psalm_client = None
-    _psalm_service = None
+    get_config.cache_clear()
+    get_workspace_repository.cache_clear()
+    get_codeql_client.cache_clear()
+    get_linguist_client.cache_clear()
+    get_workspace_service.cache_clear()
+    get_linguist_service.cache_clear()
+    get_codeql_service.cache_clear()
+    
+    get_semgrep_client.cache_clear()
+    get_semgrep_service.cache_clear()
+    get_bandit_client.cache_clear()
+    get_bandit_service.cache_clear()
+    get_gosec_client.cache_clear()
+    get_gosec_service.cache_clear()
+    get_brakeman_client.cache_clear()
+    get_brakeman_service.cache_clear()
+    get_pmd_client.cache_clear()
+    get_pmd_service.cache_clear()
+    get_psalm_client.cache_clear()
+    get_psalm_service.cache_clear()
